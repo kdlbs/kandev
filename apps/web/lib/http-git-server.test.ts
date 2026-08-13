7afd0c53c2095a7cd4051508823f38179fc649e2
ayattara <alassane.yattara@savoirfairelinux.com>
2026-08-13 03:31:00 +0000

    test: run HTTP Git fixture test in Node environment

--- apps/web/lib/http-git-server.test.ts
+++ apps/web/lib/http-git-server.test.ts
@@ -1,3 +1,4 @@
+// @vitest-environment node
 import { expect, test } from "vitest";
 import { startHTTPGitFixture } from "../e2e/helpers/http-git-server";
 import { execFileSync } from "node:child_process";
