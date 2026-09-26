74713d88951ca7f38d280087292e57f3f38a4995
Carlos Florêncio <carlosmflorencio@gmail.com>
2026-09-26 14:10:40 +0100

    chore: enforce runs and state dependency boundaries (#2234)

--- scripts/architecture_lint/rules/__init__.py
+++ scripts/architecture_lint/rules/__init__.py
@@ -1,9 +1,20 @@
 """Explicit registry of architecture rules."""
 
 from .frontend_root_state_cast import RULE as FRONTEND_ROOT_STATE_CAST
+from .frontend_state_ui_import import RULE as FRONTEND_STATE_UI_IMPORT
+from .run_scheduler_owner import RULE as RUN_SCHEDULER_OWNER
 from .inbox_history_isolation import RULE as INBOX_HISTORY_ISOLATION
 from .runtime_import import RULE as RUNTIME_IMPORT
+from .runs_office_import import RULE as RUNS_OFFICE_IMPORT
 from .task_office_import import RULE as TASK_OFFICE_IMPORT
 
 
-RULES = (RUNTIME_IMPORT, TASK_OFFICE_IMPORT, FRONTEND_ROOT_STATE_CAST, INBOX_HISTORY_ISOLATION)
+RULES = (
+    RUNTIME_IMPORT,
+    TASK_OFFICE_IMPORT,
+    FRONTEND_ROOT_STATE_CAST,
+    INBOX_HISTORY_ISOLATION,
+    FRONTEND_STATE_UI_IMPORT,
+    RUN_SCHEDULER_OWNER,
+    RUNS_OFFICE_IMPORT,
+)
